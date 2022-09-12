47f1bb8972109432789d78aa5e17f19d0a09b33f
Andrew Schmelyun <schmelyun@gmail.com>
2022-09-12 02:59:39 -0400

    Adds to the help output

--- src/Reqon.ts
+++ src/Reqon.ts
@@ -77,13 +77,26 @@ export default class Reqon {
         }
 
         if (this.args.hasOwnProperty('help')) {
-            this.drawHelp()
+            this.drawTitle()
+                .drawHelp()
         }
 
         return this
     }
     drawHelp(): void {
-        console.log(chalk.yellow.bold("Usage: ") + chalk.white("reqon"))
+        console.log(chalk.cyan.bold("Description: ") + chalk.white("effortlessly intercept and inspect http requests."))
+        console.log("")
+        console.log(chalk.cyan.bold("Usage: ") + chalk.white("reqon ") + chalk.gray("[options]"))
+        console.log("")
+        console.log(chalk.cyan.bold("Options: "))
+        console.log(chalk.white("  --port=<port>") + "               " + chalk.gray("sets the port to listen for incoming requests"))
+        console.log(chalk.white("  --dashboard-port=<port>") + "     " + chalk.gray("sets the port the dashboard is available on"))
+        console.log(chalk.white("  --save-max=<number>") + "         " + chalk.gray("changes the max number of entries saved locally"))
+        console.log(chalk.white("  --save-file=<path>") + "          " + chalk.gray("changes the filepath used for local db, json ext required"))
+        console.log(chalk.white("  --no-dashboard") + "              " + chalk.gray("disables the dashboard, --dashboard-port is ignored"))
+        console.log(chalk.white("  --no-save") + "                   " + chalk.gray("disables saving locally, --save-file + --save-max ignored"))
+        console.log(chalk.white("  --help") + "                      " + chalk.gray("what you're seeing right now :)"))
+        console.log("")
         process.exit()
     }
     drawTitle(): Reqon {
