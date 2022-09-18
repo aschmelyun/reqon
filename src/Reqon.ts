81e47c90d7a4e0eea01a51e22255ce3b9e22b1f0
Andrew Schmelyun <schmelyun@gmail.com>
2022-09-17 23:42:41 -0400

    Removes unused calls to drawTitle and drawHelp

--- src/Reqon.ts
+++ src/Reqon.ts
@@ -104,44 +104,6 @@ export default class Reqon {
     }
     
     /**
-     * Fired off if the --help argument was passed in
-     * Draws a block of text for usage basics and the list of options
-     */
-    drawHelp(): void {
-        console.log(chalk.cyan.bold("Description: ") + chalk.white("effortlessly intercept and inspect http requests."))
-        console.log("")
-        console.log(chalk.cyan.bold("Usage: ") + chalk.white("reqon ") + chalk.gray("[options]"))
-        console.log("")
-        console.log(chalk.cyan.bold("Options: "))
-        console.log(chalk.white("  --port=<port>") + "               " + chalk.gray("sets the port to listen for incoming requests"))
-        console.log(chalk.white("  --dashboard-port=<port>") + "     " + chalk.gray("sets the port the dashboard is available on"))
-        console.log(chalk.white("  --save-max=<number>") + "         " + chalk.gray("changes the max number of entries saved locally"))
-        console.log(chalk.white("  --save-file=<path>") + "          " + chalk.gray("changes the filepath used for local db, json ext required"))
-        console.log(chalk.white("  --no-dashboard") + "              " + chalk.gray("disables the dashboard, --dashboard-port is ignored"))
-        console.log(chalk.white("  --no-save") + "                   " + chalk.gray("disables saving locally, --save-file + --save-max ignored"))
-        console.log(chalk.white("  --help") + "                      " + chalk.gray("what you're seeing right now :)"))
-        console.log("")
-        process.exit()
-    }
-    
-    /**
-     * Some sick ascii art
-     * @returns Reqon
-     */
-    drawTitle(): Reqon {
-        console.clear()
-        console.log('')
-        console.log(chalk.cyan.bold("┏━┓ ┏━━┓ ┏━━┓ ┏━━┓ ┏━┓ "))
-        console.log(chalk.cyan.bold("┃┏┛ ┃┃━┫ ┃┏┓┃ ┃┏┓┃ ┃┏┓┓"))
-        console.log(chalk.cyan.bold("┃┃  ┃┃━┫ ┃┗┛┃ ┃┗┛┃ ┃┃┃┃"))
-        console.log(chalk.cyan.bold("┗┛  ┗━━┛ ┗━┓┃ ┗━━┛ ┗┛┗┛"))
-        console.log(chalk.cyan.bold("           ┗┛          "))
-        console.log('')
-
-        return this
-    }
-
-    /**
      * Fires off the listener for the main reqon express webserver
      * Listens to any urls, using any http method, and passes everything to the interceptor handler
      * @returns Reqon
